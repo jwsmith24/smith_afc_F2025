48c85dbcbd46c20a8275d8d6e46cac42be1a8b09
Jacob Smith <jacob.w.smith97.mil@army.mil>
2025-09-22 10:25:07 -0500

    factor out complex test cases into own describe block

--- palindrome_test/test/isPalindrome.test.ts
+++ palindrome_test/test/isPalindrome.test.ts
@@ -1,29 +1,31 @@
 import {isPalindrome} from "../src/isPalindrome";
 
-describe("isPalindrome function", () => {
+describe("isPalindrome function basic cases", () => {
     it("returns true for simple palindromes", () => {
         expect(isPalindrome("racecar")).toBe(true);
         expect(isPalindrome("madam")).toBe(true);
     });
-
-    it("returns true for complex palindromes", () => {
-        expect(isPalindrome("Never odd or even")).toBe(true);
-        expect(isPalindrome("A man, a plan, a canal: Panama")).toBe(true);
-        expect(isPalindrome("r:a.-)ceCAR/ . , []{}/"))
-    })
 
     it("returns false for words that aren't palindromes", () => {
         expect(isPalindrome("postgresql")).toBe(false);
         expect(isPalindrome("typescript")).toBe(false);
     });
 
-    it("returns false for complex phrases that aren't palindromes", () => {
-        expect(isPalindrome("postgres iS, a. super::rad database")).toBe(false);
-        expect(isPalindrome("wow::/. this phrase is NOT a ./;'[]]]][] palindrome"))
-    })
-
     it("ignores case and whitespace", () => {
         expect(isPalindrome("RAceCAr")).toBe(true);
         expect(isPalindrome("\t mAdAm     \t")).toBe(true);
     });
 });
+
+describe("isPalindrome function complex cases", () => {
+    it("returns true for complex palindromes", () => {
+        expect(isPalindrome("Never odd or even")).toBe(true);
+        expect(isPalindrome("A man, a plan, a canal: Panama")).toBe(true);
+        expect(isPalindrome("r:a.-)ceCAR/ . , []{}/"))
+    });
+
+    it("returns false for complex phrases that aren't palindromes", () => {
+        expect(isPalindrome("postgres iS, a. super::rad database")).toBe(false);
+        expect(isPalindrome("w;ow::/. this ph[ra}se is NOT a ./;'[]]]][] palindrome"))
+    });
+})
