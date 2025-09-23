610a337670166f59eedd4af18df602793ce911c6
Jacob Smith <jacob.w.smith97.mil@army.mil>
2025-09-22 19:50:19 -0500

    add table component, bring in menu item array

--- react_restaurant/src/components/MenuTable.tsx
+++ react_restaurant/src/components/MenuTable.tsx
@@ -1,4 +1,53 @@
-// use table component
-// display 5 items
-// four cols: image, item name, desc, price
-// data must come from parent
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+type MenuItem = {
+  name: string;
+  description: string;
+  price: number;
+  image: string;
+};
+type MenuTableProps = {
+  menuItems: MenuItem[];
+};
+
+export default function MenuTable({ menuItems }: MenuTableProps) {
+  return (
+    <div className={"border px-8 py-4 rounded shadow hover:cursor-pointer"}>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Image</TableHead>
+            <TableHead>Item Name</TableHead>
+            <TableHead>Description</TableHead>
+            <TableHead>Price</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {menuItems.map((item) => {
+            return (
+              <TableRow>
+                <TableCell>
+                  <img
+                    src={item.image}
+                    alt={item.description}
+                    className={"w-12 h-12"}
+                  />
+                </TableCell>
+                <TableCell>{item.name}</TableCell>
+                <TableCell>{item.description}</TableCell>
+                <TableCell>{item.price}</TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
--- react_restaurant/src/view/MenuView.tsx
+++ react_restaurant/src/view/MenuView.tsx
@@ -1,3 +1,49 @@
+import MenuTable from "@/components/MenuTable.tsx";
+import tacoIcon from "@/assets/icons/taco-icon.png";
+import burritoIcon from "@/assets/images/burritos-icon.png";
+import fajitasIcon from "@/assets/images/fajitas3.jpeg";
+import aquafrescaIcon from "@/assets/images/aqua-fresca-icon.png";
+import horchataIcon from "@/assets/images/horchata-icon.png";
+
 export default function MenuView() {
-  return <div>MENU PLACEHOLDER</div>;
+  const menuItems = [
+    {
+      name: "Tacos",
+      description:
+        "Personally recommended by everyone you know, genuinely SCRUMPTIOUS",
+      price: 5.99,
+      image: tacoIcon,
+    },
+    {
+      name: "Fajitas",
+      description: "All ratings say: exceptionally PALATABLE",
+      price: 6.99,
+      image: fajitasIcon,
+    },
+    {
+      name: "Burritos",
+      description: "These are quite DELECTABLE. Order them rn or else.",
+      price: 6.99,
+      image: burritoIcon,
+    },
+    {
+      name: "Aqua Fresca",
+      description:
+        "Moderately DIVINE. Flavors: Strawberry, Cantaloupe, or Prickly Pear.",
+      price: 3.99,
+      image: aquafrescaIcon,
+    },
+    {
+      name: "Horchata",
+      description: "Cinnamon included at no extra charge.",
+      price: 3.99,
+      image: horchataIcon,
+    },
+  ];
+  return (
+    <div className={"flex flex-col gap-6 items-center justify-center h-full"}>
+      <h1 className={"text-4xl font-bold"}>Taco && Taco Specials</h1>
+      <MenuTable menuItems={menuItems} />
+    </div>
+  );
 }
